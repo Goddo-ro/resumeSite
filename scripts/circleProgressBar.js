eb95454201686f9faf862d12c7f960ec19a9b159
Goddo-ro <r0manovic1@yandex.ru>
2022-06-27 18:16:03 +0300

    Добавлены карточки с навыками

--- scripts/circleProgressBar.js
+++ scripts/circleProgressBar.js
@@ -3,7 +3,7 @@ console.log("hello")
 let options = {
     startAngle: -1.55,
     size: 150,
-    value: 0.85,
+    value: 0.70,
     fill: {gradient: ["#a445b2", "#fa4299"]}
 }
 
@@ -13,8 +13,17 @@ function(event, progress, stepValue) {
     $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
 });
 $(".python .bar").circleProgress({
-    value: 0.70,
+    value: 0.80,
+});
+$(".django .bar").circleProgress({
+    value: 0.50,
 });
 $(".js .bar").circleProgress({
+    value: 0.60,
+});
+$(".с .bar").circleProgress({
+    value: 0.30,
+});
+$(".english .bar").circleProgress({
     value: 0.60,
 });
