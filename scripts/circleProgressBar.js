44a8f6b5790124e14d9b137a7b16eea9f4ca7c8f
Goddo-ro <r0manovic1@yandex.ru>
2022-06-27 21:42:28 +0300

    Добавлен черновой carousel в секцию проектов

--- scripts/circleProgressBar.js
+++ scripts/circleProgressBar.js
@@ -1,29 +1,51 @@
-console.log("hello")
+function isInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
 
-let options = {
-    startAngle: -1.55,
-    size: 150,
-    value: 0.70,
-    fill: {gradient: ["#00ADB5", "#458EF1"]}
+    );
+}
+
+function startAnimation() {
+    let options = {
+        startAngle: -1.55,
+        size: 150,
+        value: 0.70,
+        fill: {gradient: ["#00ADB5", "#458EF1"]}
+    }
+
+    $(".circle .bar").circleProgress(options).on('circle-animation-progress',
+    function(event, progress, stepValue) {
+        $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+    });
+    $(".python .bar").circleProgress({
+        value: 0.80,
+    });
+    $(".django .bar").circleProgress({
+        value: 0.50,
+    });
+    $(".js .bar").circleProgress({
+        value: 0.60,
+    });
+    $(".с .bar").circleProgress({
+        value: 0.30,
+    });
+    $(".english .bar").circleProgress({
+        value: 0.60,
+    });
 }
 
 
-$(".circle .bar").circleProgress(options).on('circle-animation-progress',
-function(event, progress, stepValue) {
-    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
-});
-$(".python .bar").circleProgress({
-    value: 0.80,
-});
-$(".django .bar").circleProgress({
-    value: 0.50,
-});
-$(".js .bar").circleProgress({
-    value: 0.60,
-});
-$(".с .bar").circleProgress({
-    value: 0.30,
-});
-$(".english .bar").circleProgress({
-    value: 0.60,
-});
+let animStarted = false;
+document.addEventListener('scroll', function () {
+    if (animStarted == false) {
+        const bar = $(".circle .bar")[0];
+        if (isInViewport(bar)) {
+            startAnimation();
+            animStarted = true;
+        }
+    }
+})
