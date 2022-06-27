0e0d23442a7f475ec306652f20e5fb5d3cecf43e
Goddo-ro <r0manovic1@yandex.ru>
2022-06-27 18:07:12 +0300

    Добавлены круговые сайдбары в секцию скилов

---
+++ scripts/circleProgressBar.js
@@ -0,0 +1,20 @@
+console.log("hello")
+
+let options = {
+    startAngle: -1.55,
+    size: 150,
+    value: 0.85,
+    fill: {gradient: ["#a445b2", "#fa4299"]}
+}
+
+
+$(".circle .bar").circleProgress(options).on('circle-animation-progress',
+function(event, progress, stepValue) {
+    $(this).parent().find("span").text(String(stepValue.toFixed(2).substr(2)) + "%");
+});
+$(".python .bar").circleProgress({
+    value: 0.70,
+});
+$(".js .bar").circleProgress({
+    value: 0.60,
+});
